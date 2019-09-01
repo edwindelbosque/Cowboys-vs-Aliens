d8e960ab6c7626645aee096ed0f73fb8ee832225
Zoe <zoekharvey@gmail.com>
2019-08-31 19:26:28 -0600

    Add test and function in Game class to select a random survey

--- test/game-test.js
+++ test/game-test.js
@@ -1,10 +1,11 @@
 import chai from 'chai';
 import Game from '../src/Game';
+import data from '../data/surveys.js'
 const expect = chai.expect;
 
 let game;
 beforeEach(() => {
-  game = new Game();
+  game = new Game(data);
 });
 
 describe('Game', () => {
@@ -17,5 +18,29 @@ describe('Game', () => {
     expect(game).to.be.an.instanceOf(Game);
   });
 
+  it('should hold all survey data', () => {
+    expect(game.data).to.deep.equal(data);
+  });
+
+  it('should select a random survey', () => {
+    game.selectSurvey(3)
+    expect(game.currentSurvey.length).to.equal(4);
+  });
+
+  it('should sort the answers from highest respondents to lowest', () => {
+    game.selectSurvey(3)
+    expect(game.currentSurvey).to.eql([   
+      { id: 3, question: 'Name A Good Gift For Someone Who Is Always Late.' },
+      { answer: 'Watch', respondents: 58, surveyId: 3 },
+      { answer: 'Alarm Clock', respondents: 34, surveyId: 3 },
+      { answer: 'Calendar', respondents: 3, surveyId: 3 },
+    ])
+  });
+
+
+
 
 }); // <------ end of describe block
+
+
+
