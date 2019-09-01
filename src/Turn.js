a2782037dccdc89601d2aedf350cd96bb9aaa6de
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-09-01 13:29:00 -0600

    Adds functionality and appropriate tests to find questions and its respective answers as an array

--- src/Turn.js
+++ src/Turn.js
@@ -1,17 +1,47 @@
 class Turn {
-  constructor(currentRound, currentSurvey, currentPlayer, correctGuesses) {
+  constructor(currentRound) {
     this.currentRound = currentRound;
-    this.currentSurvey = currentSurvey;
-    this.currentPlayer = currentPlayer;
+    // this.currentSurvey = currentRound.surveyQuestion;
+    // this.currentAnswers = currentRound.answers
+    // this.currentPlayer = currentPlayer;
     this.correctGuesses = [];
   }
-}
+
+  identifyQuestion() {
+    return this.currentRound.surveyQuestion.id;
+  }
+
+  identifyAnswerInfo() {
+    return this.currentRound.answers
+  }
+
+  identifyCorrectAnswers() {
+    return this.identifyAnswerInfo().map(answerInfo => {
+      return answerInfo.answer
+    })
+  }
 
 
-//checks if guess is correct
-//if correct, updates score 
-//Give feedback
-//Toggle the player
+  capitalizeGuess(guess) {
+    let capitalizedWords = guess.toLowerCase().split(' ').map(word => {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    })
+    return capitalizedWords.join(' ') 
+  }
+
+  // checkGuess()
+
+  // ?constructor(currentRound, currentSurvey, currentPlayer)
+  //checks if guess is correct
+  //if correct, updates score 
+  //Give feedback
+  //Toggle the player
+
+} //<---- end of Turn class
+
+
+
+
 
 
 export default Turn;
--- test/turn-test.js
+++ test/turn-test.js
@@ -1,10 +1,22 @@
 import chai from 'chai';
 import Turn from '../src/Turn';
+import Round from '../src/Round';
+import Game from '../src/Game';
+import data from '../data/surveys.js'
 const expect = chai.expect;
 
-let turn;
+let turn, round, game;
 beforeEach(() => {
-  turn = new Turn();
+  turn = new Turn(round);
+  round = new Round(
+    { id: 1, question: 'If You Drew Homer Simpson’s Name In A Secret Santa Exchange, What Would You Buy Him?' },
+    [
+      { answer: 'Beer', respondents: 67, surveyId: 1 },
+      { answer: 'Bowling Ball', respondents: 5, surveyId: 1 },
+      { answer: 'Donuts', respondents: 24, surveyId: 1 }
+    ]
+  );
+  // game = new Game(surveys);
 });
 
 describe('Turn', () => {
@@ -16,6 +28,32 @@ describe('Turn', () => {
   it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceOf(Turn);
   });
+
+  it('should know ID of survey question', () => {
+    expect(turn.identifyQuestion()).to.eql(1)
+  });
+
+  it('should find answers for prompted question', () => {
+    expect(turn.identifyAnswerInfo()).to.eql(
+      [
+        { answer: 'Beer', respondents: 67, surveyId: 1 },
+        { answer: 'Bowling Ball', respondents: 5, surveyId: 1 },
+        { answer: 'Donuts', respondents: 24, surveyId: 1 }
+      ])
+  });
+
+  it('should return correct answers from answer info', () => {
+    expect(turn.identifyCorrectAnswers()).to.eql(['Beer', 'Bowling Ball', 'Donuts'])
+  });
+
+  it('should capitalize guesses', () => {
+    expect(turn.capitalizeGuess('bOwLiNg BaLL')).to.equal('Bowling Ball')
+  });
+
+
+  // it('should check to see if guess was correct', () => {
+  //   expect(turn.checkGuess('Bowling Ball')).to.equal(true);
+  // });
 
 
 }); // <------ end of describe block
