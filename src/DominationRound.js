2ef9ac0fb74cf28c78b5817969da9f6325644b73
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-08-31 20:46:51 -0600

    Add a couple methods to Round class file

--- src/DominationRound.js
+++ src/DominationRound.js
@@ -1,8 +1,6 @@
 class DominationRound extends Round {
   constructor(survey, answers, multiplier) {
     super(survey, answers);
-    this.survey = survey;
-    this.answers = answers;
     this.multiplier = multiplier;
   }
 
