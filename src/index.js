61515bbeb2e87a4a26b456427757df2dfd0d6fc1
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-09-01 23:07:02 -0600

    Fix turing-image.jpg error and discarded photo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
 import './images/start-page.gif';
 import './images/alien-icon.png';
 import './images/cowboy-icon.png';
