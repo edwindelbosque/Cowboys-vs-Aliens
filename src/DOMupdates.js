bebe0690ce614bad2b783c0c4e9ff73374fccc8d
Zoe <zoekharvey@gmail.com>
2019-09-03 16:20:25 -0600

    Add DOM updates file and connected to Game class and test

---
+++ src/DOMupdates.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+
+const DOMupdates = {
+  showWinner(player) {
+    $('').text(player.name)
+    $('').text(player.score)
+  }
+
+ 
+
+}
+
+
+
+export default DOMupdates;
