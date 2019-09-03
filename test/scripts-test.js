ef0ccc95a10a4e0e03aa413b1e11220dd6384c2f
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-09-03 11:39:53 -0600

    Add spice test file for scripts.js

---
+++ test/scripts-test.js
@@ -0,0 +1,18 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+const spies = require('chai-spies');
+chai.use(spies);
+
+let startGameButton;
+
+const index = require('../src/index');
+
+describe('index', () => {
+
+  startGameButton.on('click', () => {
+    $("html").delay(250).animate({ scrollTop: main.offset().top }, 1000);
+
+  })
+
+}); // <----- end of describe block
